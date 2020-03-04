e395dd90dc64324e7a6c7dc61eb0c6f7ed2b69a5
AntoineMkr <ant.mecker@hotmail.fr>
2020-03-04 17:54:24 +0100

    :construction: adding stuff

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+import axios from "axios"
+
+
 console.log("hello word");
+
+function add (a, b) {
+    return a+b
+}
+
+/* document.querySelector('#nomDeLelement')
+    .addEventListener("click",function writeToChat (event) {
+        var message = document.querySelector("#message").value
+        var discussionElt = document.querySelector(".discussion")//. c'est pour dire que c'est une classe
+        var nickname= document.querySelector("#nickname").value
+
+        axios.post('/api/messafe', {
+            message: 'Michel',
+            nickname: 'Michel'
+        })
+    }) */
+
+//Les 7 types sur js, le dernier a partir de juin 2020
+//Number
+//String
+//Boolean
+//Null
+//Undefined
+//Symbol
+//Objet
+//ES2020 => BigIn
+
+//Pas de classe en js
+
+
+//camelCase     variable
+//PascalCase    fonction
+//kebab-case    
+//snake_case
