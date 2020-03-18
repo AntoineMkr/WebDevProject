5aba188c442133a6697719a9f97054f24448dbbc
Lou <lou.prevot@outlook.fr>
2020-03-18 14:35:50 +0100

    changement nom

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/api/version', function getVersion (req, res) {
 
 app.use(function (req, res, next) {
   res.status(404)
-  res.sendFile(path.join(__dirname, '../client/dist/404.html'))
+  res.sendFile(path.join(__dirname, '../client/404.html'))
 })
 
 app.listen(port, () => console.log(`Listening ! Go to http://localhost:${port}!`))
