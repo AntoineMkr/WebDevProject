92e108fd533ba844731eff709f50438220149eed
Lou <lou.prevot@outlook.fr>
2020-03-18 14:22:38 +0100

    nv  projet

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express') // je peux appeler express parce que je l'ai installé dans les dépendances
+const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.use('/',function (req, res) {
-  res.status(200).send('index')
-});
+app.use(express.static('../client/dist'))
 
-app.use('*',function (req, res) {
-    res.status(404).send("Error 404, Sorry")
-  });
+app.get('/api/version', function getVersion (req, res) {
+  res.send('0.0.2')
+})
 
- 
+app.use(function (req, res, next) {
+  res.status(404)
+  res.sendFile(path.join(__dirname, '../client/dist/404.html'))
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Go to http://localhost:${port}!`))
+app.listen(port, () => console.log(`Listening ! Go to http://localhost:${port}!`))
