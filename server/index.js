81fb4979d3f5dae279458b09d2387d2a29b32e5e
AntoineMkr <ant.mecker@hotmail.fr>
2020-03-18 14:43:31 +0100

    :rocket: adding files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('../client/dist'))
 
-
-app.get('/api/version', function getVersion(req,res){
-    res.send('0.0.2')
+app.get('/api/version', function getVersion (req, res) {
+  res.send('0.0.2')
 })
 
-app.listen(
-    port, () => console.log(`Listening! Goto http://localhost:${port}`)
-    )
+app.use(function (req, res, next) {
+  res.status(404)
+  res.sendFile(path.join(__dirname, '../client/dist/404.html'))
+})
+
+app.listen(port, () => console.log(`Listening ! Go to http://localhost:${port}!`))
